724c5da4d5d8fb7304b77a2f5e0a38310c519db6
MacroUniverse <270174408@qq.com>
2023-03-06 10:49:30 -0600

    add mkl64-2022.1.0

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 echo ":: initializing SLISC0-libs-x64-ubuntu18.04"
 
 # recover split files due to github filesize limitation
-FILES="flint2-2.9.0/lib/libflint.a"
+FILES="flint2-2.9.0/lib/libflint.a mkl64-2022.1.0/lib/libmkl_core.a"
 for FILE in $FILES
 do
 if [[ ! -f "$FILE" ]]; then
@@ -31,3 +31,4 @@ $ADD/lapack-static-3.10.1
 $ADD/mpfr-4.1.0
 $ADD/mplapack-2.0.1
 $ADD/sqlite-3.40.0
+$ADD/mkl64-2022.1.0
